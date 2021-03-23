e4bc5ae99796168ed9814ebf3cebdcffc99088e7
ahmadfaridulazmi <ahmadfaridulazmi@gmail.com>
2021-03-23 21:39:38 +0800

    refactor code

--- lib/volcanic/imageman/v1/image.rb
+++ lib/volcanic/imageman/v1/image.rb
@@ -25,40 +25,29 @@ class Volcanic::Imageman::V1::Image
         cache_duration: cache_duration,
         cacheable: cacheable
       ).tap do |img|
-        img._upload_and_create(resolve_file(attachable))
+        img._upload_and_create(attachable)
       end
     end
 
     def fetch_by(reference: nil, uuid: nil)
-      raise ArgumentError, 'Expect either reference or uuid, both got nil' unless reference || uuid
+      validate_persisted_var(reference, uuid)
+      new(reference: resolve_reference(reference), uuid: uuid).tap(&:reload)
+    end
 
-      ref = reference.is_a?(Volcanic::Imageman::V1::Reference) ? reference.md5_hash : reference
-      new(reference: ref, uuid: uuid).tap(&:reload)
+    def update(attachable:, reference: nil, uuid: nil)
+      validate_persisted_var(reference, uuid)
+      new(reference: resolve_reference(reference), uuid: uuid).update_file(attachable)
     end
 
     def delete(reference: nil, uuid: nil)
-      raise ArgumentError, 'Expect either reference or uuid, both got nil' unless reference || uuid
-
-      ref = reference.is_a?(Volcanic::Imageman::V1::Reference) ? reference.md5_hash : reference
-      new(reference: ref, uuid: uuid).delete
+      validate_persisted_var(reference, uuid)
+      new(reference: resolve_reference(reference), uuid: uuid).delete
     end
 
     private
 
-    # return base64 string
-    def resolve_file(attachable)
-      raise ArgumentError, 'Expect a value attachable, both got nil' if attachable.nil?
-
-      case attachable
-      when Hash
-        Base64.strict_encode64(attachable.fetch(:io)&.read)
-      else
-        if attachable.respond_to?('read')
-          Base64.strict_encode64(attachable.read)
-        else
-          attachable # a base64 file
-        end
-      end
+    def validate_persisted_var(ref, uuid)
+      raise ArgumentError, 'Expect either reference or uuid, both got nil' if ref.nil? && uuid.nil?
     end
 
     def resolve_details(ref, name)
@@ -70,6 +59,10 @@ class Volcanic::Imageman::V1::Image
 
         [ref, name]
       end
+    end
+
+    def resolve_reference(ref)
+      ref.is_a?(Volcanic::Imageman::V1::Reference) ? ref.md5_hash : ref
     end
   end
 
@@ -90,10 +83,10 @@ class Volcanic::Imageman::V1::Image
     conn.delete(persisted_path) && true
   end
 
-  def update_file(base_64_string)
+  def update_file(attachable)
     return false unless persisted?
 
-    conn.post(persisted_path) { |req| req.body = { file: base_64_string } }
+    conn.post(persisted_path) { |req| req.body = { file: resolve_file(attachable) } }
     true
   end
 
@@ -102,11 +95,12 @@ class Volcanic::Imageman::V1::Image
   end
 
   # Dont use this method, use class method +create+ instead
-  def _upload_and_create(base_64_string)
+  def _upload_and_create(attachable)
+    file = resolve_file(attachable)
     res = conn.post API_PATH do |req|
       req.body = {
         fileName: name,
-        file: base_64_string,
+        file: file,
         reference: reference,
         cache_duration: cache_duration,
         cacheable: cacheable
@@ -127,5 +121,21 @@ class Volcanic::Imageman::V1::Image
 
   def persisted_path
     "#{API_PATH}/#{uuid || reference}"
+  end
+
+  # return base64 string
+  def resolve_file(attachable)
+    raise ArgumentError, 'Expect a value of attachable, got nil' if attachable.nil?
+
+    case attachable
+    when Hash
+      Base64.strict_encode64(attachable.fetch(:io)&.read)
+    else
+      if attachable.respond_to?('read')
+        Base64.strict_encode64(attachable.read)
+      else
+        attachable # a base64 file
+      end
+    end
   end
 end
